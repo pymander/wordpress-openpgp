e5489c162ff6bcbc59a69ed057b4a0e0968670eb
Erik L. Arneson <dybbuk@lnouv.com>
2014-11-25 13:07:17 -0800

    Wrappers for OpenPGP.js that get it hacky in a webpage
    Yeah, this is hacky. But it works.

---
+++ js/init.js
@@ -0,0 +1,45 @@
+/** Wrapper functions for OpenPGP.js
+
+    These functions just help tie OpenPGP.js to some jQuery stuff so
+    they can affect the DOM.
+*/
+
+/** Load a public key from a URI. The file at the end of the URI
+    should just be an ASCII armored key file. */
+function loadPublicKey(keyURI) {
+    var response, publicKey;
+
+    // We need this to be an asynchronous call.
+    jQuery.ajax({
+        url: keyURI,
+        success: function (data) {
+            publicKey = openpgp.key.readArmored(data);
+            alert("PK: " + publicKey);
+        },
+        dataType: 'html',
+        async: false
+    });
+
+    return publicKey;
+}
+
+/** Encrypt a textarea. Pass in the id of a textarea and an
+    openpgp.key object, maybe loaded by loadPublicKey() above. */
+function encryptTextarea(textareaId, publicKey) {
+    var plaintext = jQuery('#' + textareaId).val();
+    var encrypted = jQuery('#' + textareaId).attr('data-encrypted');
+
+    if (encrypted) {
+        alert("You've already encrypted this textarea.");
+        return false;
+    }
+
+    var pgpMessage = openpgp.encryptMessage(publicKey.keys, plaintext);
+
+    jQuery('#' + textareaId).val(pgpMessage);
+    jQuery('#' + textareaId).attr('data-encrypted', true);
+
+    return true;
+}
+
+// Testing/staging
