930852ddbc504248a32869f6a25e661dd05fe245
Erik L. Arneson <dybbuk@lnouv.com>
2014-12-01 10:59:28 -0800

    Release version 1.1
    This is a same-day release because I needed to get this thing ready
    for submission to the WordPress plugin directory.

--- wordpress-openpgp.php
+++ wordpress-openpgp.php
@@ -5,7 +5,7 @@
 /*
 Plugin Name: OpenPGP for Textareas
 Description: Provide encryption using OpenPGP.js for textareas via buttons and callbacks and such.
-Version: 1.0
+Version: 1.1
 Author: Erik L. Arneson
 Author URI: http://www.arnesonium.com/
 License: GPLv2 or later
