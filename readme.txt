e2bd7b664d763632235b1f396fab5b0cc20199b9
arnesonium <arnesonium@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-01 20:44:04 +0000

    Merge branch 'master' of github.com:pymander/wordpress-openpgp
    git-svn-id: http://plugins.svn.wordpress.org/openpgp-form-encryption/trunk@1036214 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,97 @@
+=== OpenPGP Form Encryption for WordPress ===
+Contributors: arnesonium
+Tags: forms, encryption, pgp, gnupg, openpgp
+Requires at least: 4.0
+Tested up to: 4.0.1
+Stable tag: v1.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+OpenPGP public key encryption for any textarea with a shortcode button.
+
+== Description ==
+
+This plugin uses [OpenPGP.js](http://openpgpjs.org/) to provide public key encryption for a
+textarea. It is most useful for any kind of text area that will be
+submitted via email or over an unsecured network connection.
+
+== Installation ==
+
+To install this plugin, follow these directions:
+
+1. Download the latest zip file from [the releases page](https://github.com/pymander/wordpress-openpgp/releases).
+1. Next, load up your WordPress blog’s dashboard, and go to **Plugins > Add New**.
+1. Upload the zip file.
+1. Click **Activate**.
+
+== Frequently Asked Questions ==
+
+= How do I get a public key? =
+
+OpenPGP for WordPress requires access to an OpenPGP public key in
+ASCII armored form. For more information on key generation and
+OpenPGP, I recommend the [Email Self-Defense website](https://emailselfdefense.fsf.org/en/), which has
+instructions for multiple operating systems.
+
+== Screenshots ==
+
+1. A textarea that has been encrypted, including a view of the "Encrypt" button.
+
+== Changelog ==
+
+= 1.0 =
+* First release.
+
+= 0.9 =
+* Development pre-release.
+
+== Upgrade Notice ==
+
+== Usage ==
+This plugin provides a simple shortcode which you can add to your
+forms. To use the shortcode, you must first upload your ASCII-armored
+public key to your blog’s media section. Note that the public key must
+reside on the same server as your blog.
+
+The `cryptbutton` shortcode takes the following arguments.
+
+**keyid**
+The media ID of your ASCII-armored public key. Either this or keyurl
+are required.
+
+**keyurl**
+The URL for your ASCII-armored public key. Either this argument or
+keyid are required.
+
+**textarea**
+The HTML ID for the textarea element to be encrypted. This argument is
+required.
+
+**class**
+Optional. Specify additional CSS classes for the button element.
+
+**style**
+Optional. Specify additional CSS styles for the button element.
+
+**text**
+Optional. Specify the button text. This defaults to “Encrypt”.
+
+= Example =
+
+I use this plugin with the [Jetpack for WordPress](http://jetpack.me/) contact form. You can
+see an example of the output on my [Contact page](http://arnesonium.com/contact/). The WordPress code
+looks something like this:
+
+```
+[contact-form subject='ARNESONIUM CONTACT']
+[contact-field label='Name' type='name' required='1'/]
+[contact-field label='Email' type='email' required='1'/]
+[contact-field label='Phone' type='text'/]
+[contact-field label='Comment' type='textarea' required='1'/]
+[cryptbutton textarea="contact-form-comment" keyid=42]
+[/contact-form]
+```
+
+Line 6 displays the cryptbutton usage. Note that I changed some
+elements of this example to make things clearer. You will need to play
+with layout and CSS to get things looking nice.
---
+++ wordpress-openpgp.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * @package OpenPGP
+ */
+/*
+Plugin Name: OpenPGP for Textareas
+Description: Provide encryption using OpenPGP.js for textareas via buttons and callbacks and such.
+Version: 1.1
+Author: Erik L. Arneson
+Author URI: http://www.arnesonium.com/
+License: GPLv2 or later
+
+    Copyright 2014 Erik L. Arneson (email : earneson@arnesonium.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+defined('ABSPATH') or die("No script kiddies please!");
+
+// This isn't actually being called all the time. We don't need to load all of the OpenPGP.js library on every
+// page. It's too big for that.
+function openpgp_enqueue_scripts () {
+    wp_enqueue_script('openpgp', plugins_url('js/openpgp.min.js', __FILE__));
+    wp_enqueue_script('openpgp-init', plugins_url('js/init.js', __FILE__));
+}
+
+function openpgp_cryptbutton_header ()
+{
+    $output = <<<EOT
+<script type="text/javascript">
+jQuery(document).ready(function() {
+    jQuery(document).on('click', '.cryptbutton', function (event) {
+        // Get textArea and public key info.
+        var textareaId = jQuery(this).attr('data-textarea-id');
+        var pubkeyUri = jQuery(this).attr('data-pubkey-uri');
+
+        publicKey = loadPublicKey(pubkeyUri);
+        encryptTextarea(textareaId, publicKey);
+    });
+});
+</script>
+EOT;
+
+    echo $output;
+
+}
+
+function openpgp_cryptbutton_shortcode ($atts = array(), $content = null, $tag)
+{
+    // We load our JavaScript only if needed. It's a pretty hefty library.
+    // Possible bug with this method: https://core.trac.wordpress.org/ticket/11526
+    if (FALSE == wp_script_is('openpgp', 'enqueued')) {
+        wp_enqueue_script('openpgp', plugins_url('js/openpgp.min.js', __FILE__));
+        wp_enqueue_script('openpgp-init', plugins_url('js/init.js', __FILE__));
+    }
+
+    $args = shortcode_atts(
+        array(
+            'textarea' => null,
+            'keyurl' => null,
+            'keyid' => null,
+            'class'  => null,
+            'style' => null,
+            'text'  => 'Encrypt'
+        ),
+        $atts
+    );
+
+    $keyurl = $args['keyurl'];
+
+    // Easier is to just pass a media ID.
+    if (0 < (int)$args['keyid']) {
+        $keyurl = wp_get_attachment_url((int)$args['keyid']);
+    }
+
+    return sprintf("<button type=\"button\" class=\"cryptbutton %s\" %sdata-pubkey-uri=\"%s\" data-textarea-id=\"%s\">%s</button>",
+                   $args['class'],
+                   (isset($args['style']) ? "style=\"" . $args['style'] . "\" " : ''),
+                   $keyurl,
+                   $args['textarea'],
+                   $args['text']
+    );
+}
+
+// Set up stuff to print headers and things.
+//add_action('wp_enqueue_scripts', 'openpgp_enqueue_scripts');
+add_action('wp_head', 'openpgp_cryptbutton_header');
+add_shortcode('cryptbutton', 'openpgp_cryptbutton_shortcode');
