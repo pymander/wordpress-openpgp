0bab103b83ee689a88c94b4a343c81d71703baae
Erik L. Arneson <dybbuk@lnouv.com>
2014-11-30 21:41:16 -0800

    Rename main PHP file to match plugin name

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+# Installation
+
+To install this plugin from source, please download it from the Github
+project page.
+
 # Acknowledgements
 
 This plugin uses OpenPGP.js, which can be found here: http://openpgpjs.org/
@@ -5,3 +10,7 @@ This plugin uses OpenPGP.js, which can be found here: http://openpgpjs.org/
 A version is included with the plugin so that this software doesn't
 have to worry about API changes and so that end users don't have to
 worry about downloading multiple pieces of software.
+
+# TODO
+
+See http://codex.wordpress.org/Plugin_Submission_and_Promotion
--- wordpress-openpgp.php
+++ wordpress-openpgp.php
@@ -28,6 +28,8 @@ License: GPLv2 or later
 
 defined('ABSPATH') or die("No script kiddies please!");
 
+// This isn't actually being called all the time. We don't need to load all of the OpenPGP.js library on every
+// page. It's too big for that.
 function openpgp_enqueue_scripts () {
     wp_enqueue_script('openpgp', plugins_url('js/openpgp.min.js', __FILE__));
     wp_enqueue_script('openpgp-init', plugins_url('js/init.js', __FILE__));
@@ -56,13 +58,21 @@ EOT;
 
 function openpgp_cryptbutton_shortcode ($atts = array(), $content = null, $tag)
 {
+    // We load our JavaScript only if needed. It's a pretty hefty library.
+    // Possible bug with this method: https://core.trac.wordpress.org/ticket/11526
+    if (FALSE == wp_script_is('openpgp', 'enqueued')) {
+        wp_enqueue_script('openpgp', plugins_url('js/openpgp.min.js', __FILE__));
+        wp_enqueue_script('openpgp-init', plugins_url('js/init.js', __FILE__));
+    }
+
     $args = shortcode_atts(
         array(
             'textarea' => null,
             'keyurl' => null,
             'keyid' => null,
             'class'  => null,
-            'style' => null
+            'style' => null,
+            'text'  => 'Encrypt'
         ),
         $atts
     );
@@ -74,15 +84,16 @@ function openpgp_cryptbutton_shortcode ($atts = array(), $content = null, $tag)
         $keyurl = wp_get_attachment_url((int)$args['keyid']);
     }
 
-    return sprintf("<button type=\"button\" class=\"cryptbutton %s\" %sdata-pubkey-uri=\"%s\" data-textarea-id=\"%s\">Encrypt</button>",
+    return sprintf("<button type=\"button\" class=\"cryptbutton %s\" %sdata-pubkey-uri=\"%s\" data-textarea-id=\"%s\">%s</button>",
                    $args['class'],
                    (isset($args['style']) ? "style=\"" . $args['style'] . "\" " : ''),
                    $keyurl,
-                   $args['textarea']
+                   $args['textarea'],
+                   $args['text']
     );
 }
 
 // Set up stuff to print headers and things.
-add_action('wp_enqueue_scripts', 'openpgp_enqueue_scripts');
+//add_action('wp_enqueue_scripts', 'openpgp_enqueue_scripts');
 add_action('wp_head', 'openpgp_cryptbutton_header');
 add_shortcode('cryptbutton', 'openpgp_cryptbutton_shortcode');
