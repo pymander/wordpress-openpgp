9f1d4d3f95d240c472cc40f18bcd7cf8feaec2fb
arnesonium <arnesonium@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-13 17:43:32 +0000

    Load OpenPGP.js by version number
    The version number in the file is important for things like caching and
    CDN stuff.
    
    git-svn-id: http://plugins.svn.wordpress.org/openpgp-form-encryption/trunk@1159795 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wordpress-openpgp.php
+++ wordpress-openpgp.php
@@ -32,7 +32,7 @@ defined('ABSPATH') or die("No script kiddies please!");
 // page. It's too big for that.
 function openpgp_enqueue_scripts () {
     if (FALSE == wp_script_is('openpgp', 'enqueued')) {
-        wp_enqueue_script('openpgp', plugins_url('js/openpgp.min.js', __FILE__));
+        wp_enqueue_script('openpgp', plugins_url('js/openpgp.0.10.0.min.js', __FILE__));
         wp_enqueue_script('openpgp-init', plugins_url('js/init.js', __FILE__));
     }
 }
