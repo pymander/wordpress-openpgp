7582498270165141ea5560f5523dc9abbdc63b5a
Erik L. Arneson <earneson@arnesonium.com>
2018-01-17 16:00:52 -0800

    Fix indentation in js/init.js

--- js/init.js
+++ js/init.js
@@ -7,70 +7,70 @@
 /** Load a public key from a URI. The file at the end of the URI
     should just be an ASCII armored key file. */
 function loadPublicKey(keyURI) {
-    var response, publicKey;
+  var response, publicKey;
 
-    // We need this to be an asynchronous call.
-    jQuery.ajax({
-        url: keyURI,
-        success: function (data) {
-            publicKey = openpgp.key.readArmored(data);
-        },
-        dataType: 'html',
-        async: false
-    });
+  // We need this to be an asynchronous call.
+  jQuery.ajax({
+    url: keyURI,
+    success: function (data) {
+      publicKey = openpgp.key.readArmored(data);
+    },
+    dataType: 'html',
+    async: false
+  });
 
-    return publicKey;
+  return publicKey;
 }
 
 /** Encrypt a textarea. Pass in the id of a textarea and an
     openpgp.key object, maybe loaded by loadPublicKey() above. */
 function encryptTextarea(textareaId, publicKey) {
-    var area = jQuery('#' + textareaId);
-    var plaintext;
+  var area = jQuery('#' + textareaId);
+  var plaintext;
 
-    // If the browser can't do OpenPGP.js, we need to alert the user.
-    if (!window.crypto.getRandomValues) {
-        alert("We're sorry, but your browser doesn't support OpenPGP.js.");
-        return false;
-    }
+  // If the browser can't do OpenPGP.js, we need to alert the user.
+  if (!window.crypto.getRandomValues) {
+    alert("We're sorry, but your browser doesn't support OpenPGP.js.");
+    return false;
+  }
 
-    // If we didn't get a textareaId passed to us, find a suitable candidate and use that.
-    if (!textareaId) {
-        area = jQuery('#cryptbutton').parents('form').find('textarea');
-        //alert("found area " + area);
-    }
+  // If we didn't get a textareaId passed to us, find a suitable candidate and use that.
+  if (!textareaId) {
+    area = jQuery('#cryptbutton').parents('form').find('textarea');
+    //alert("found area " + area);
+  }
 
-    // Finally, if we don't have an encryptable area at this point, let's bug out.
-    if (!area) {
-        alert("Unable to find encryptable textarea.");
-        return false;
-    }
+  // Finally, if we don't have an encryptable area at this point, let's bug out.
+  if (!area) {
+    alert("Unable to find encryptable textarea.");
+    return false;
+  }
 
-    if (area.attr('data-encrypted')) {
-        alert("You've already encrypted this textarea.");
-        return false;
-    }
+  if (area.attr('data-encrypted')) {
+    alert("You've already encrypted this textarea.");
+    return false;
+  }
 
-    // Now, let's disable the textarea while we work.
-    area.prop('readonly',true);
+  // Now, let's disable the textarea while we work.
+  area.prop('readonly',true);
 
-    // Fetch plaintext.
-    plaintext = area.val();
+  // Fetch plaintext.
+  plaintext = area.val();
 
-    var pgpMessagePromise = openpgp.encryptMessage(publicKey.keys, plaintext);
+  var pgpMessagePromise = openpgp.encryptMessage(publicKey.keys, plaintext);
 
-    pgpMessagePromise.then(function (ctext) {
-        // This initial newline makes sure the encrypted text starts on its own line.
-        area.val("\n" + ctext);
-        area.attr('data-encrypted', true);
-    }, function (err) {
-        alert(err);
-    });
+  pgpMessagePromise.then(function (ctext) {
+    // This initial newline makes sure the encrypted text starts on its own line.
+    area.val("\n" + ctext);
+    area.attr('data-encrypted', true);
+  }, function (err) {
+    alert(err);
+  });
 
-    // If you want to make the textarea writeable after, uncomment this.
-    //area.prop('readonly',false);
+  // If you want to make the textarea writeable after, uncomment this.
+  //area.prop('readonly',false);
 
-    return true;
+  return true;
 }
 
 // Testing/staging
