afb1b55dd7f2b9fae8eb1863c15a4ae9d367242a
Erik L. Arneson <earneson@arnesonium.com>
2018-01-17 16:04:08 -0800

    Upgrade to OpenPGP 2.3.5

--- wordpress-openpgp.php
+++ wordpress-openpgp.php
@@ -32,7 +32,7 @@ defined('ABSPATH') or die("No script kiddies please!");
 // page. It's too big for that.
 function openpgp_enqueue_scripts () {
     if (FALSE == wp_script_is('openpgp', 'enqueued')) {
-        wp_enqueue_script('openpgp', plugins_url('js/openpgp.1.2.0.min.js', __FILE__));
+        wp_enqueue_script('openpgp', plugins_url('js/openpgp.2.3.5.min.js', __FILE__));
         wp_enqueue_script('openpgp-init', plugins_url('js/init.js', __FILE__));
     }
 }
